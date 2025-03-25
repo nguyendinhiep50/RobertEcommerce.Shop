f39311e73d4178141fb7b394c642c59eba66a77d
hiep <nguyendinhhiep317@gmail.com>
2025-03-25 22:21:49 +0700

    add library aspire

---
+++ RobertEcommerce.Shop.ManagerProject.AppHost/Extensions.cs
@@ -0,0 +1,55 @@
+﻿using Aspire.Hosting.Lifecycle;
+
+namespace RobertEcommerce.Shop.ManagerProject.AppHost;
+
+internal static class Extensions
+{
+    /// <summary>
+    /// Adds a hook to set the ASPNETCORE_FORWARDEDHEADERS_ENABLED environment variable to true for all projects in the application.
+    /// </summary>
+    public static IDistributedApplicationBuilder AddForwardedHeaders(this IDistributedApplicationBuilder builder)
+    {
+        builder.Services.TryAddLifecycleHook<AddForwardHeadersHook>();
+        return builder;
+    }
+
+    private class AddForwardHeadersHook : IDistributedApplicationLifecycleHook
+    {
+        public Task BeforeStartAsync(DistributedApplicationModel appModel, CancellationToken cancellationToken = default)
+        {
+            foreach (var p in appModel.GetProjectResources())
+            {
+                p.Annotations.Add(new EnvironmentCallbackAnnotation(context =>
+                {
+                    context.EnvironmentVariables["ASPNETCORE_FORWARDEDHEADERS_ENABLED"] = "true";
+                }));
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+    /// <summary>
+    /// Configures eShop projects to use Ollama for text embedding and chat.
+    /// </summary>
+    public static IDistributedApplicationBuilder AddOllama(this IDistributedApplicationBuilder builder,
+        IResourceBuilder<ProjectResource> catalogApi,
+        IResourceBuilder<ProjectResource> webApp)
+    {
+        var ollama = builder.AddOllama("ollama")
+            .WithDataVolume()
+            .WithGPUSupport()
+            .WithOpenWebUI();
+        var embeddings = ollama.AddModel("embedding", "all-minilm");
+        var chat = ollama.AddModel("chat", "llama3.1");
+
+        catalogApi.WithReference(embeddings)
+            .WithEnvironment("OllamaEnabled", "true")
+            .WaitFor(embeddings);
+        webApp.WithReference(chat)
+            .WithEnvironment("OllamaEnabled", "true")
+            .WaitFor(chat);
+
+        return builder;
+    }
+}
---
+++ RobertEcommerce.Shop.ManagerProject.AppHost/Program.cs
@@ -0,0 +1,35 @@
+using RobertEcommerce.Shop.ManagerProject.AppHost;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddForwardedHeaders();
+
+var redis = builder.AddRedis("redis");
+var rabbitMq = builder.AddRabbitMQ("eventbus")
+    .WithLifetime(ContainerLifetime.Persistent);
+var postgres = builder.AddPostgres("postgres")
+    .WithPgWeb()
+    .WithImage("ankane/pgvector")
+    .WithImageTag("latest")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var catalogDb = postgres.AddDatabase("catalogdb");
+var identityDb = postgres.AddDatabase("identitydb");
+var orderDb = postgres.AddDatabase("orderingdb");
+var webhooksDb = postgres.AddDatabase("webhooksdb");
+
+var launchProfileName = ShouldUseHttpForEndpoints() ? "http" : "https";
+
+builder.Build().Run();
+
+// For test use only.
+// Looks for an environment variable that forces the use of HTTP for all the endpoints. We
+// are doing this for ease of running the Playwright tests in CI.
+static bool ShouldUseHttpForEndpoints()
+{
+    const string EnvVarName = "ESHOP_USE_HTTP_ENDPOINTS";
+    var envValue = Environment.GetEnvironmentVariable(EnvVarName);
+
+    // Attempt to parse the environment variable value; return true if it's exactly "1".
+    return int.TryParse(envValue, out int result) && result == 1;
+}
